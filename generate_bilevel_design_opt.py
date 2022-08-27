5269af80771983d4dbcec03917274fef6f201643
song970407 <song970407@kaist.ac.kr>
2022-08-27 07:10:53 -0700

    ready to generate data

---
+++ generate_bilevel_design_opt.py
@@ -0,0 +1,133 @@
+import os
+import yaml
+import pickle
+
+import numpy as np
+import torch
+
+from src.env.HeatDiffusion import HeatDiffusionSystem
+from src.utils.get_graph import generate_full_graph
+from src.utils.get_position import generate_random_position
+from src.utils.fix_seed import fix_seed
+
+
+def generate_heat_diffusion_data(env_config, data_generation_config):
+    num_cells = env_config['num_cells']
+    dt = env_config['dt']
+    epsilon = env_config['epsilon']
+    domain_range = env_config['domain_range']
+
+    traj_len = data_generation_config['traj_len']
+    num_train_traj = data_generation_config['num_train_traj']
+    num_val_traj = data_generation_config['num_val_traj']
+    num_test_traj = data_generation_config['num_test_traj']
+    num_sensors_bound = data_generation_config['num_sensors_bound']
+    num_heaters_bound = data_generation_config['num_heaters_bound']
+    action_min = data_generation_config['action_min']
+    action_max = data_generation_config['action_max']
+    train_data_saved_path = data_generation_config['train_data_saved_path']
+    val_data_saved_path = data_generation_config['val_data_saved_path']
+    test_data_saved_path = data_generation_config['test_data_saved_path']
+    seed_num = data_generation_config['seed_num']
+    fix_seed(seed_num)
+
+    def _generate_data(num_traj, saved_path):
+        data = []
+        for _ in range(num_traj):
+            episode = {}
+            num_sensors = np.random.randint(num_sensors_bound[0], num_sensors_bound[1] + 1)
+            num_heaters = np.random.randint(num_heaters_bound[0], num_heaters_bound[1] + 1)
+            state_pos, action_pos = generate_random_position(domain_range, num_sensors, num_heaters, epsilon)
+            env = HeatDiffusionSystem(num_cells=num_cells,
+                                      dt=dt,
+                                      epsilon=epsilon,
+                                      domain_range=domain_range,
+                                      action_min=action_min,
+                                      action_max=action_max,
+                                      state_pos=state_pos,
+                                      action_pos=action_pos)
+            state_trajectory, action_trajectory = env.generate_random_trajectory(traj_len)
+            torch_state_pos = torch.from_numpy(state_pos).float()
+            torch_action_pos = torch.from_numpy(action_pos).float()
+            graph = generate_full_graph(torch_state_pos, torch_action_pos)
+
+            episode = {
+                'state_pos': state_pos,
+                'action_pos': action_pos,
+                'state_trajectory': state_trajectory,
+                'action_trajectory': action_trajectory,
+                'graph': graph
+            }
+            data.append(episode)
+        pickle.dump(data, open(saved_path, 'wb'))
+
+    _generate_data(num_train_traj, train_data_saved_path)
+    _generate_data(num_val_traj, val_data_saved_path)
+    _generate_data(num_test_traj, test_data_saved_path)
+
+
+def generate_heater_location(bilevel_design_opt_config):
+    num_x_list = bilevel_design_opt_config['num_x_list']
+    num_heaters_list = bilevel_design_opt_config['num_heaters_list']
+    num_graphs = bilevel_design_opt_config['num_repeats']
+    domain_range = bilevel_design_opt_config['domain_range']
+    epsilon = bilevel_design_opt_config['epsilon']
+    num_targets = bilevel_design_opt_config['num_targets']
+    target_length = bilevel_design_opt_config['target_length']
+    max_heatups = bilevel_design_opt_config['max_heatups']
+    target_range = bilevel_design_opt_config['target_range']
+    initial_target = bilevel_design_opt_config['initial_target']
+    saved_dir = bilevel_design_opt_config['saved_dir']
+    seed_num = bilevel_design_opt_config['seed_num']
+    fix_seed(seed_num)
+
+    for num_x in num_x_list:
+        num_sensors = num_x ** 2
+        for num_heaters in num_heaters_list:
+            problem = {
+                'state_pos': [],
+                'action_pos': [],
+                'graph': []
+            }
+            state_pos_list = []
+            action_pos_list = []
+            graph_list = []
+            for graph_idx in range(num_graphs):
+                state_pos, action_pos = generate_random_position(domain_range, num_sensors, num_heaters, epsilon)
+                torch_state_pos = torch.from_numpy(state_pos).float()
+                torch_action_pos = torch.from_numpy(action_pos).float()
+                graph = generate_full_graph(torch_state_pos, torch_action_pos)
+                problem['state_pos'].append(state_pos)
+                problem['action_pos'].append(action_pos)
+                problem['graph'].append(graph)
+            pickle.dump(problem, open('{}/problem_{}_{}.pkl'.format(saved_dir, num_x, num_heaters), 'wb'))
+
+    target_values_list = []
+    target_times_list = []
+    target = {
+        'target_values': [],
+        'target_times': []
+    }
+    for idx in range(num_targets):
+        num_heatups = np.random.randint(1, max_heatups + 1, size=num_targets)
+        each_length = int(target_length / num_heatups[idx])
+        anneal_temp = np.sort(np.random.uniform(target_range[0], target_range[1], size=(num_heatups[idx])))
+        heatup_ratio = np.random.randint(2, each_length - 2, size=(num_heatups[idx]))
+        target_values = [initial_target]
+        target_times = []
+        for i in range(num_heatups[idx]):
+            target_values.append(anneal_temp[i])
+            target_values.append(anneal_temp[i])
+            target_times.append(heatup_ratio[i])
+            target_times.append(each_length - heatup_ratio[i])
+        target['target_values'].append(target_values)
+        target['target_times'].append(target_times)
+    pickle.dump(target, open('{}/target.pkl'.format(saved_dir), 'wb'))
+
+
+if __name__ == '__main__':
+    env_config = yaml.safe_load(open('config/env/env_config.yaml', 'r'))
+    data_generation_config = yaml.safe_load(open('config/data/data_generation_config.yaml', 'r'))
+    bilevel_design_opt_config = yaml.safe_load(open('config/data/bilevel_design_opt_config.yaml', 'r'))
+    generate_heat_diffusion_data(env_config, data_generation_config)
+    generate_heater_location(bilevel_design_opt_config)
--- src/utils/preprocess_data.py
+++ src/utils/preprocess_data.py
@@ -57,7 +57,6 @@ def _preprocess_data(states,  # T+1 x state_dim
     return history_xs_list, history_us_list, us_list, ys_list, graph_list, idx_torch
 
 
-
 def preprocess_data(data, data_preprocessing_config):
     scaler = data_preprocessing_config['scaler']
     scale = data_preprocessing_config['scale']
@@ -75,64 +74,3 @@ def preprocess_data(data, data_preprocessing_config):
     future_states = []
 
     # for i, episode in enumerate(data): # Start from here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    if type(states) != list:
-        states = [states]
-    if type(actions) != list:
-        actions = [actions]
-    if type(graphs) != list:
-        graphs = [graphs]
-    num_dataset = int(len(states) * data_ratio)
-    history_xs_list, history_us_list, us_list, ys_list, graph_list = [], [], [], [], []
-    idx_list = []
-    for idx in range(num_dataset):
-        state = states[idx]
-        action = actions[idx]
-        num_state = state.shape[1]
-        num_action = action.shape[1]
-        if state.shape[0] == action.shape[0]:
-            state = torch.cat([torch.zeros(receding_history, num_state).to(state.device), state], dim=0)
-            action = torch.cat([torch.zeros(receding_history - 1, num_action).to(action.device), action], dim=0)
-        else:
-            state = torch.cat([torch.zeros(receding_history - 1, num_state).to(state.device), state], dim=0)
-            action = torch.cat([torch.zeros(receding_history - 1, num_action).to(action.device), action], dim=0)
-        state = state.transpose(0, 1).unsqueeze(dim=-1)  # state_dim x T+1 x 1
-        action = action.transpose(0, 1).unsqueeze(dim=-1)  # action_dim x T x 1
-        state = (state - state_scaler[0]) / (state_scaler[1] - state_scaler[0])
-        action = (action - action_scaler[0]) / (action_scaler[1] - action_scaler[0])
-        T = state.shape[1]  # length of trajectory
-        history_xs, history_us, us, ys = [], [], [], []
-        for itr in range(T - receding_history - receding_horizon + 1):
-            history_xs.append(state[:, itr:itr + receding_history])
-            history_us.append(action[:, itr:itr + receding_history - 1])
-            us.append(action[:, itr + receding_history - 1:itr + receding_history + receding_horizon - 1])
-            ys.append(state[:, itr + receding_history:itr + receding_history + receding_horizon])
-            idx_list.append(torch.tensor([idx, itr]))
-        history_xs = torch.stack(history_xs).to(device)  # D x state_dim x receding_history
-        history_us = torch.stack(history_us).to(device)  # D x action_dim x receding_history-1
-        us = torch.stack(us).to(device)  # D x action_dim x receding_horizon
-        ys = torch.stack(ys).to(device)  # D x state_dim x receding_horizon
-        history_xs_list.append(history_xs)
-        history_us_list.append(history_us)
-        us_list.append(us)
-        ys_list.append(ys)
-        graph_list.append(graphs[idx].to(device))
-    idx_torch = torch.stack(idx_list).int().to(device)
-    return history_xs_list, history_us_list, us_list, ys_list, graph_list, idx_torch
