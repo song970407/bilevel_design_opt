1e808da3812e1fccb8422e9bbe98e01716fd816d
song970407 <song970407@kaist.ac.kr>
2022-09-15 00:08:37 -0700

    fix error

--- compute_bilevel_design_opt_performance.py
+++ compute_bilevel_design_opt_performance.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
         os.makedirs('bilevel_opt_result/optimal/{}'.format(solver_name))
 
     opt_result = pickle.load(open('bilevel_opt_result/{}/{}_{}.pkl'.format(solver_name, num_x, num_heaters), 'rb'))
-    state_pos = pickle.load(open('data/bilevel_design_opt/problem_{}_{}.pkl'.format(num_x, num_heaters), 'rb'))['state_pos']
+    state_pos = pickle.load(open('data/bilevel_design_opt/problem_{}_{}.pkl'.format(num_x, num_heaters), 'rb'))['state_pos'][0]
     action_pos = opt_result['opt_action_pos']
     x_trajectory_list, u_trajectory_list, log_trajectory_list = run_optimal_control(mpc_config,
                                                                                     env_config,
